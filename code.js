08193292d0001e062a7d4f90dfe72425eb00869d
yury-n <yury.nedomolkin@gmail.com>
2022-12-06 16:08:09 +0800

    implementation for consumers

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 const fixes = {};
 const fillStyleId = figma.currentPage.selection[0]["fillStyleId"];
 fillStyleId && console.log("fillStyleId", fillStyleId);
+const strokeStyleId = figma.currentPage.selection[0]["strokeStyleId"];
+strokeStyleId && console.log("strokeStyleId", strokeStyleId);
 // figma.currentPage.selection[0]["fills"] = [];
 // figma.currentPage.selection[0]["fillStyleId"] = null;
 // 22830:330105
@@ -25,67 +27,26 @@ fillStyleId && console.log("fillStyleId", fillStyleId);
 // figma.viewport.scrollAndZoomIntoView([node]);
 // Library
 const brokenToWorkingStyleId = {
-    "6d40ba5ad182a999a501768e80b1da4b8fa231d4": "fe3bfda22784624ebc72547c3e526d0e1ca315cb",
-    "8b3d6e4daa17e7d56c780fb58dd28a4adb70eb1d": "5ec24d05c4efa98ac2ff9cdee6857b7f0405063e",
-    c342e7fb9cc5c59584b61609ecefacee1bddbf2d: "fd656735b86b49267988cc53ad10827d9a47b7bb",
-    "1848bb26c7723c0b7840cca4d36d309baac88081": "ab6827d4e2d7c06ab4dae4b1876bdb139fa85100",
-    c5e9024659b558a52cdf821365fa72a7895a2bc5: "0bd8b8de81f606f927efe9b3228925f5ffbdba98",
-    "0b3742e5ef4dd74d2c3add8ab7d5c21bc7fb7325": "13375713e3901ee5214d164a4a9e31a7fd3a7eed",
-    "125e704d24fea3aec06abcfdec96f2b43e69269b": "63e72c497fa75f2af2b59931910782c539d7da2b",
-    d38de9e0bded16283a5c64cf0b0325e87c26d21f: "af57bb2d5eed62a3aa8c01282b23dd814de8c3d0",
-    a20363eb30f7e4ed4773f032c80f4b21b0bdb973: "1992330df46952f14e71cb15785a62976f8d89e2",
-    af74307c5846decb73d9634ecf7823c151188a66: "4c7db22717863ce7c68189d75ba54f18723c0481",
-    c4d491963e2ac1ffbd70ea4a44b033fb71afc5a2: "7e82cefd2a139615513b5cb9899663a2f26a4e7c",
-    "7966e8e3b8d35c90f032128ed0e244e61a4f31a2": "b2072b5c73acff740cb6b392df7f5d360d733d7b",
-    "0efec6c9d368e0491a45b465fa2f7c80953a1402": "226ed7e8e6ac3f112dcc8a379001c52992481a9f",
-    "4b04dda597c97a70956f312c2b2cc20e7753892b": "302b56b4201d9b861bd6aea326400b37e5ff75d6",
+    d8ae12c0b0046098a0f214e0e5abf6495dea924e: "fe3bfda22784624ebc72547c3e526d0e1ca315cb",
+    "18a985f4b1101e073271d5fa03ac4d2f539d0ccd": "5ec24d05c4efa98ac2ff9cdee6857b7f0405063e",
+    "981900c8eb2f7c54311731d4413455017d45d462": "fd656735b86b49267988cc53ad10827d9a47b7bb",
+    "6523715b284e8f1d83aebadc7c8ce59bcf2137e2": "ab6827d4e2d7c06ab4dae4b1876bdb139fa85100",
+    "33d8ce3c082bb23d23e4256016944b2a293c074e": "0bd8b8de81f606f927efe9b3228925f5ffbdba98",
+    "88ee31cc2a490364aff0fb7447374099f0bfd4fb": "13375713e3901ee5214d164a4a9e31a7fd3a7eed",
+    fe16b44fb1716b249c637ebd9e3fd0267675cd1d: "63e72c497fa75f2af2b59931910782c539d7da2b",
+    c36c9b72400242c94b98100aaca4225baea4f6e0: "af57bb2d5eed62a3aa8c01282b23dd814de8c3d0",
+    bb5f389cdc643ec9627e10bac1ea558a9eed9403: "1992330df46952f14e71cb15785a62976f8d89e2",
+    "319dd5cf2a088cb8319f4a574b7a7ebab09cb14c": "7e82cefd2a139615513b5cb9899663a2f26a4e7c",
+    "3f97bc09911e343e57c30f2c9838f8f4bedb9c06": "b2072b5c73acff740cb6b392df7f5d360d733d7b",
+    "7234c71275680c67b30d121961bb60e5c15ff298": "226ed7e8e6ac3f112dcc8a379001c52992481a9f",
+    "115304dfd0242da8554621b59d8f0e9c0e89098f": "302b56b4201d9b861bd6aea326400b37e5ff75d6",
 };
-const oppositesDarkToLight = {
-    fe3bfda22784624ebc72547c3e526d0e1ca315cb: "af57bb2d5eed62a3aa8c01282b23dd814de8c3d0",
-    "5ec24d05c4efa98ac2ff9cdee6857b7f0405063e": "1992330df46952f14e71cb15785a62976f8d89e2",
-    fd656735b86b49267988cc53ad10827d9a47b7bb: "4c7db22717863ce7c68189d75ba54f18723c0481",
-    ab6827d4e2d7c06ab4dae4b1876bdb139fa85100: "7e82cefd2a139615513b5cb9899663a2f26a4e7c",
-    "0bd8b8de81f606f927efe9b3228925f5ffbdba98": "b2072b5c73acff740cb6b392df7f5d360d733d7b",
-    "13375713e3901ee5214d164a4a9e31a7fd3a7eed": "226ed7e8e6ac3f112dcc8a379001c52992481a9f",
-    "63e72c497fa75f2af2b59931910782c539d7da2b": "302b56b4201d9b861bd6aea326400b37e5ff75d6",
-};
-const oppositesLightToDark = {
-    af57bb2d5eed62a3aa8c01282b23dd814de8c3d0: "fe3bfda22784624ebc72547c3e526d0e1ca315cb",
-    "1992330df46952f14e71cb15785a62976f8d89e2": "5ec24d05c4efa98ac2ff9cdee6857b7f0405063e",
-    "4c7db22717863ce7c68189d75ba54f18723c0481": "fd656735b86b49267988cc53ad10827d9a47b7bb",
-    "7e82cefd2a139615513b5cb9899663a2f26a4e7c": "ab6827d4e2d7c06ab4dae4b1876bdb139fa85100",
-    b2072b5c73acff740cb6b392df7f5d360d733d7b: "0bd8b8de81f606f927efe9b3228925f5ffbdba98",
-    "226ed7e8e6ac3f112dcc8a379001c52992481a9f": "13375713e3901ee5214d164a4a9e31a7fd3a7eed",
-    "302b56b4201d9b861bd6aea326400b37e5ff75d6": "63e72c497fa75f2af2b59931910782c539d7da2b",
-};
-function createInstances() {
-    const instancesFrame = figma.currentPage.children.find((node) => node.name === "_all_instances");
-    let processedCompsCount = 0;
-    function processComponentNode(componentNode) {
-        processedCompsCount++;
-        // if (processedCompsCount > 3) return;
-        const instance = componentNode.createInstance();
-        instancesFrame.appendChild(instance);
-    }
-    figma.currentPage.children.forEach((node) => {
-        if (node.type === "COMPONENT_SET") {
-            node.children.forEach((variant) => {
-                if (variant.type === "COMPONENT") {
-                    processComponentNode(variant);
-                }
-            });
-        }
-        else if (node.type === "COMPONENT") {
-            processComponentNode(node);
-        }
-    });
-}
 let fixesCount = 0;
 const generatedFixes = {};
-function generateFixesMapFromNode(node) {
+function applyFixToNode(node) {
     // if (fixesCount > 3) return;
     if (node.children) {
-        node.children.forEach(generateFixesMapFromNode);
+        node.children.forEach(applyFixToNode);
     }
     const { id, fillStyleId } = node;
     if (!fillStyleId) {
@@ -96,7 +57,13 @@ function generateFixesMapFromNode(node) {
     }
     const fillStyleKey = fillStyleId.split(":")[1].split(",")[0];
     if (fillStyleKey && brokenToWorkingStyleId[fillStyleKey]) {
-        generatedFixes[id] = brokenToWorkingStyleId[fillStyleKey];
+        const workingStyleKey = brokenToWorkingStyleId[fillStyleKey];
+        if (fetchedStyles[workingStyleKey]) {
+            node.fillStyleId = fetchedStyles[workingStyleKey].id;
+        }
+        else {
+            console.log("no style fetched", { fillStyleKey });
+        }
         fixesCount++;
     }
 }
@@ -108,46 +75,21 @@ async function fetchStyle(styleKey) {
     return new Promise((resolve) => resolve(null));
 }
 async function fetchAllStyles() {
-    const processedStyles = [];
-    const requiredStyles = [...new Set(Object.values(fixes))];
+    const requiredStyles = [...new Set(Object.values(brokenToWorkingStyleId))];
     const promises = requiredStyles.map(fetchStyle);
     return await Promise.all(promises);
-}
-function applyFixToNode(node) {
-    if (node.children) {
-        node.children.forEach(applyFixToNode);
-    }
-    if (fixes[node.id]) {
-        const styleKey = fixes[node.id];
-        console.log("id", node.id);
-        if (fetchedStyles[styleKey]) {
-            node.fillStyleId = fetchedStyles[styleKey].id;
-            console.log("applied", { styleKey });
-        }
-        else {
-            console.log("no style fetched", { styleKey });
-        }
-    }
-}
-function generateAllFixes() {
-    figma.currentPage.children.forEach((node) => {
-        if (node.type !== "COMPONENT_SET" && node.type !== "COMPONENT") {
-            return;
-        }
-        generateFixesMapFromNode(node);
-    });
-    console.log(JSON.stringify(generatedFixes));
 }
 async function applyFixes() {
     await fetchAllStyles();
     console.log({ fetchedStyles });
-    figma.currentPage.children.forEach(applyFixToNode);
+    if (figma.currentPage.selection.length) {
+        figma.currentPage.selection.forEach(applyFixToNode);
+    }
+    else {
+        figma.currentPage.children.forEach(applyFixToNode);
+    }
     console.log("done");
+    figma.closePlugin();
 }
-// Step 1
-// createInstances();
-// Step 2
-// generateAllFixes();
-// Step 3
-// applyFixes();
-figma.closePlugin();
+applyFixes();
+// figma.closePlugin();
